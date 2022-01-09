d3941dae9a3031680174cd60bbc5f529892d4e96
yejuSeok <82350743+yedio@users.noreply.github.com>
2022-01-09 19:09:09 +0900

    Add:Typescript 적용 (#1)

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function Main() {
-  return <Wrapper>Main</Wrapper>;
+  return <Wrapper>main</Wrapper>;
 }
 
 const Wrapper = styled.div`
-height:500px;
+  height: 500px;
 `;
